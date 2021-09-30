bc5f9941338c080cc6f7d69faf8479c20b2076ad
Ammar Yasir <ammar29.ay@gmail.com>
2021-09-30 17:40:02 +0300

    🍕 find and fix the issue and immutable arr

--- task.js
+++ task.js
@@ -1,44 +1,36 @@
-let numbers = [
-    5,
-    8,
-    0,
-    1,
-    9,
-    11,
-    15,
-    16
-];
+let numbers = [5, 8, 0, 1, 9, 11, 15, 16];
+let mutable = numbers.slice();
 
-console.log("Original numbers list: ", numbers)
+console.log("Original numbers list: ", mutable);
 
-for(let i = 0 ; i < numbers.length ; i++)
-{
-    for(let j = 0 ; j < numbers.length - 1 ; j++)
-    {
-        if(numbers[i] > numbers[j + 1])
-        {
-            let temp = numbers[j];
-            numbers[j] = numbers[j + 1]
-            numbers[j + 1] = temp;
-        }
-    }
+function bblSortASC(arr) {
+  // firt loop for check the last item
+  for (var i = 0; i < arr.length; i++)
+    // second loop for check the acuall elem
+    // The issue here in condetion of end the loop
+    for (var j = 0; j < arr.length - i - 1; j++)
+      // If the condition is true then swap them
+      if (arr[j] > arr[j + 1]) {
+        var temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+      }
+  console.log("Numbers list After sorting: ", arr);
+}
+function bblSortDESC(arr) {
+  // firt loop for check the last item
+  for (var i = 0; i < arr.length; i++)
+    // second loop for check the acuall elem
+    // The issue here in condetion of end the loop
+    for (var j = 0; j < arr.length - i - 1; j++)
+      // If the condition is true then swap them
+      if (arr[j] < arr[j + 1]) {
+        var temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+      }
+  console.log("Numbers list After Desc sorting: ", arr);
 }
 
-console.log("Numbers list After sorting: ", numbers)
-
-for(let i = 0 ; i < numbers.length ; i++)
-{
-    for(let j = 0 ; j < numbers.length - 1 ; j++)
-    {
-        if(numbers[i] < numbers[j + 1])
-        {
-            let temp = numbers[j];
-            numbers[j] = numbers[j + 1]
-            numbers[j + 1] = temp;
-        }
-    }
-}
-
-console.log("Numbers list After Desc sorting: ", numbers)
-
-
+bblSortASC(mutable);
+bblSortDESC(mutable);
